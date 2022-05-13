b8bc8865074674f31ad9de1dc649bf6f0cfe835e
piotr-s-nowak <217728@edu.p.lodz.pl>
2022-05-13 16:37:28 +0200

    added headers for all of the tests

--- src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
+++ src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
@@ -1,6 +1,5 @@
 package edu.iis.mto.testreactor.doser;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,8 +8,45 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class MedicineDoserTest {
 
+
     @Test
-    void itCompiles() {
-        assertEquals(2, 1 + 1);
+    void shouldThrowUnavailableMedicineExceptionWhenTrayDoesNotContainMedicineSpecifiedInReceipt() {
+
     }
+
+    @Test
+    void useMethodDoseShouldInvokeLoggingMethods() {
+
+    }
+
+    @Test
+    void catchInsufficientMedicineExceptionWhenRecipeNumberGreaterThanMedicinePackageCapacity() {
+
+    }
+
+    @Test
+    void doseReceipeIsNullShouldThrowNPE() {
+
+    }
+
+    @Test
+    void shouldInvokeInfuserDispenseSpecifiedInRecipeNumberOfTimes() {
+
+    }
+
+    @Test
+    void doseMethodShouldCatchExceptionAndSetDosingResultToAnError() {
+
+    }
+
+    @Test
+    void shouldSetDosingResultToSuccessIfMedicineInRecipeWasDosedSuccessfully() {
+
+    }
+
+    @Test
+    void shouldInvokeClockWaitAfterDosing() {
+
+    }
+
 }
