9c7ba28896072b51a66837e92c0d0bbf480a22d5
piotr-s-nowak <217728@edu.p.lodz.pl>
2022-05-13 16:40:08 +0200

    added test which should throw unavailable medicine exception

--- src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
+++ src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
@@ -1,17 +1,42 @@
 package edu.iis.mto.testreactor.doser;
 
 
+import edu.iis.mto.testreactor.doser.infuser.Infuser;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 class MedicineDoserTest {
 
+    @Mock
+    Infuser infuser;
+    @Mock
+    DosageLog dosageLog;
+    @Mock
+    Clock clock;
+
+    private MedicineDoser medicineDoser;
+    private MedicinePackage medicinePackage;
+    private Receipe receipe;
+
+    @BeforeEach
+    void config() {
+        medicineDoser = new MedicineDoser(infuser, dosageLog, clock);
+        medicinePackage = new MedicinePackageBuilder().build();
+        receipe = new RecipeBuilder().build();
+    }
+
 
     @Test
     void shouldThrowUnavailableMedicineExceptionWhenTrayDoesNotContainMedicineSpecifiedInReceipt() {
-
+        MedicinePackage medicinePackage = new MedicinePackageBuilder().withMedicine(Medicine.of("drops")).build();
+        medicineDoser.add(medicinePackage);
+        assertThrows(UnavailableMedicineException.class, () -> medicineDoser.dose(receipe));
     }
 
     @Test
